ab2c1a727fc603ad4c6cfebe524ea0e3fd417ba6
Cédric BRASSEUR <Cdric.brasseur@gmail.com>
2024-02-27 14:33:34 +0100

    Fix

--- FizzBuzzProject.UnitTests/FizzBuzzTests.cs
+++ FizzBuzzProject.UnitTests/FizzBuzzTests.cs
@@ -4,36 +4,35 @@ namespace FizzBuzzProject.UnitTests
     public class FizzBuzzTests
     {
         [TestMethod]
-        public void GetOutput_WithMultipleOf3_ReturnsFizz()
-        {
-            var result = FizzBuzz.GetOutput(3);
-
-            Assert.AreEqual("Fizz", result);
-        }
-
-        [TestMethod]
-        public void GetOutput_WithMulitpleOf5_ReturnsBuzz()
-        {
-            var result = FizzBuzz.GetOutput(5);
-
-            Assert.AreEqual("Buzz", result);
-        }
-
-        [TestMethod]
-        public void GetOutput_WithOf3And5_ReturnsFizzBuzz()
+        public void GetOutput_DivisbleBy3And5_ReturnFizzBuzz()
         {
             var result = FizzBuzz.GetOutput(15);
 
             Assert.AreEqual("FizzBuzz", result);
         }
 
+        [TestMethod]
+        public void GetOutput_DivisbleBy3Only_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(6);
+
+            Assert.AreEqual("Fizz", result);
+        }
 
         [TestMethod]
-        public void GetOutput_WithNoMulitpleOf3And5_ReturnsNumberAsString()
+        public void GetOutput_DivisbleBy5Only_ReturnBuzz()
         {
-            var result = FizzBuzz.GetOutput(8);
+            var result = FizzBuzz.GetOutput(10);
 
-            Assert.AreEqual("8", result);
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [TestMethod]
+        public void GetOutput_NotDisibleBy3Or5_ReturnNumber()
+        {
+            var result = FizzBuzz.GetOutput(7);
+
+            Assert.AreEqual("7", result);
         }
     }
 }
